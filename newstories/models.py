eacfbcab05b281452f9f5a4a9ca6380edb9621ae
serhSK <sergeyskotnitskiy@gmail.com>
2018-02-27 19:17:34 +0200

    added models with Fkeys

---
+++ newstories/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from main.models import Category
+
+
+class Newstories(models.Model):
+    category = models.ForeignKey(Category, on_delete=False)
+    by = models.CharField(max_length=100, blank=True, null=True)
+    descendants = models.CharField(max_length=15, blank=True, null=True)
+    id = models.TextField(unique=True, primary_key=True, blank=True)
+    kids = models.TextField(blank=True, null=True)
+    score = models.CharField(max_length=20, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    time = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=300, blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
